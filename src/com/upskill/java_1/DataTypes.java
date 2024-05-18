64d2c51643dbae0d83057004a5f67d3afd48c998
CristianG87 <chris4evaguayas@hotmail.com>
2024-05-18 14:59:38 -0400

    first commit 5/18

---
+++ src/com/upskill/java_1/DataTypes.java
@@ -0,0 +1,29 @@
+package com.upskill.java_1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World, This is Rasel");
+		
+		int a = 2;			//5864865;
+		int b = 4;			//10864515;
+		int c = a+b;
+		System.out.println(c);
+		
+		String city = "New York";
+		String state = "NY";
+		System.out.println(city);
+		System.out.println(state);
+		
+		boolean isRaining = false;
+		System.out.println(isRaining);
+	
+		System.out.println("Hello Java students " + a * b);
+		
+		
+		String name = "Jeremy";
+		int age = 4;
+		System.out.println(name+" "+age);
+	}
+}
---
+++ src/com/upskill/java_6/Singleton.java
@@ -0,0 +1,44 @@
+package com.upskill.java_6;
+
+public class Singleton {
+	
+	// Singleton is class that can have only one object
+	
+	// Private constructor, it prevents any other class from instantiating
+	private Singleton(){
+		
+	}
+	
+	//Private static object of the class
+	private static Singleton SingletonObj = new Singleton();
+	
+	public static Singleton getInstance(){
+		return SingletonObj;
+	}
+	
+	protected static void demo1(){
+		System.out.println("Demo I method for singletone class");
+	}
+	
+	protected static void demo2(){
+		System.out.println("Demo II method for singletone class");
+	}
+	
+	protected static void demo3(){
+		System.out.println("Demo III method for singletone class");
+	}
+	
+	//git changes
+	
+	//this changes is for devReviewClass
+
+	//Changes for Git Pull request practice 
+	
+	//this changes are in devRasel branch
+	
+	//git changes using eclipse
+	
+	protected static void demo4(){
+		System.out.println("Demo IIII method for singletone class");
+	}
+}
